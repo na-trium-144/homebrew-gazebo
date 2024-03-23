4b2daac441243b396eced4dbd59a84fec751752b
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2024-03-23 13:34:18 +0900

    conflict削除

--- Formula/gazebo11.rb
+++ Formula/gazebo11.rb
@@ -46,9 +46,9 @@ class Gazebo11 < Formula
   # depends on "gdal"
   # depends on "player"
 
-  conflicts_with "gazebo7", because: "differing version of the same formula"
-  conflicts_with "gazebo9", because: "differing version of the same formula"
-  conflicts_with "gz-tools2", because: "both install bin/gz"
+  # conflicts_with "gazebo7", because: "differing version of the same formula"
+  # conflicts_with "gazebo9", because: "differing version of the same formula"
+  # conflicts_with "gz-tools2", because: "both install bin/gz"
 
   patch do
     # Fix build with graphviz 10.0
